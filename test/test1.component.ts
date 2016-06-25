6a572370b8e04b5013f86202c64a817946d54716
Allen Kim <allenhwkim@gmail.com>
2016-06-25 13:30:21 -0400

    angular2 inline template

---
+++ test/test1.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
+
+@Component({
+  templateUrl: './template.html',
+  styleUrls: ['./style1.css', './style2.css'],
+  providers: [HTTP_PROVIDERS]
+  
+})
+class Test1Component{
+  constructor() {}
+}
